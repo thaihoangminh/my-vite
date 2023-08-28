15d438ac4f096189600b2a6bda37876af8b466a3
Thái Hoàng <mr.hoangminhthai@gmail.com>
2023-08-28 22:07:31 +0700

    feat: remove theme.js

--- theme.js
+++
@@ -1,6 +0,0 @@
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const isDarkTheme = localStorage.theme === 'dark' || (!('theme' in localStorage) && prefersDarkMode);
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.documentElement.classList.toggle('dark', isDarkTheme);
-});
