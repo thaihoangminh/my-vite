4757ce2d59157aca79487d25ae2d870838defe9f
Thai Hoang <thai.hoang@positivethinking.tech>
2023-09-03 21:40:29 +0700

    chore: fix json parse

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -76,6 +76,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': ['off'],
     '@typescript-eslint/no-empty-function': ['off'],
     '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/non-nullable-type-assertion-style': ['off'],
     '@typescript-eslint/no-misused-promises': [
       'error',
       {
